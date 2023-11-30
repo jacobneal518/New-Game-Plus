dbea55c369db4534097c85dcdb1b67ee4ec28b16
Jacob Neal <jacob@binaryevolution.com>
2023-11-30 12:37:49 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/newgameplus/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.newgameplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button homeButton;
+
+    View.OnClickListener homePageListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //opens DB activity
+            Intent intent = new Intent(MainActivity.this, HomePage.class);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(homePageListener);
+    }
+}
