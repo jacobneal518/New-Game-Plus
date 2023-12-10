8b0f2fa928d26ce4f2ce5a5d1e7a8e2151ae275c
JordantheGnat <jordanmandrson@gmail.com>
2023-12-10 16:04:47 -0600

    Firebase Auth attempting, changing build, adding internet permission, some more small stuff probably

---
+++ app/src/main/java/com/example/newgameplus/LoginPage.java
@@ -0,0 +1,14 @@
+package com.example.newgameplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DatabaseReference;
+import android.os.Bundle;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
