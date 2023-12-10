7e2430aa2b80d47a644e3a08c7f0cdff99181545
JordantheGnat <jordanmandrson@gmail.com>
2023-12-10 15:44:42 -0600

    Setting Up some Buttons

--- app/src/main/java/com/example/newgameplus/MainActivity.java
+++ app/src/main/java/com/example/newgameplus/MainActivity.java
@@ -6,25 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button homeButton;
+    Button homeButton,signIn,signOut;
+    private FirebaseAuth mAuth;
+    Boolean signedIn;
+
     //
     View.OnClickListener homePageListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             //opens DB activity
+            if(signedIn==true){
             Intent intent = new Intent(MainActivity.this, HomePage.class);
             startActivity(intent);
+            }else{
+                Toast.makeText(getApplicationContext(),"Not Signed In",Toast.LENGTH_LONG).show();
+            }
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        signedIn = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         homeButton = findViewById(R.id.homeButton);
         homeButton.setOnClickListener(homePageListener);
+        mAuth = FirebaseAuth.getInstance();
     }
 }
