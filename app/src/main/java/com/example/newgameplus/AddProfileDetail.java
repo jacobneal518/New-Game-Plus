bc7ffe1a1cb7e02c862a30b8fff4f902127ffcff
JordantheGnat <jordanmandrson@gmail.com>
2023-12-11 14:59:10 -0600

    added logout button to profile details

--- app/src/main/java/com/example/newgameplus/AddProfileDetail.java
+++ app/src/main/java/com/example/newgameplus/AddProfileDetail.java
@@ -7,14 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AddProfileDetail extends AppCompatActivity {
-Button backButton;
+    Button backButton,signOut;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_profile_detail);
         backButton = findViewById(R.id.detailBack);
         backButton.setOnClickListener(backButtonListen);
+        signOut = findViewById(R.id.signOutButton);
+        signOut.setOnClickListener(signOutButtonListen);
+
 
     }
 
@@ -25,4 +31,13 @@ Button backButton;
             startActivity(intent);
                 }
             };
+    View.OnClickListener signOutButtonListen = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            Intent intent = new Intent(AddProfileDetail.this, MainActivity.class);
+            startActivity(intent);
         }
+    };
+}
